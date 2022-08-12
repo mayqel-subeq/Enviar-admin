60bdea7124f80b4fa1082709e891e74a503e5ebc
raviharyo <raffiiphone99@gmail.com>
2022-08-13 00:01:00 +0700

    add login function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import AdminPage from './pages/AdminPage';
+import { Route, Routes } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedLogin from './components/ProtectedLogin';
+import './assets/tailwind/index.css'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Routes>
+        <Route path='/' element={
+          <ProtectedRoute>
+            <AdminPage />
+          </ProtectedRoute>
+        }>
+          <Route index={true} element={<HomePage />} />
+        </Route>
+        <Route path='/login' element={
+          <ProtectedLogin>
+            <LoginPage />
+          </ProtectedLogin>
+        }></Route>
+      </Routes>
     </div>
   );
 }
